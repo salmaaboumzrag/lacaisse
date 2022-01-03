4d6d20d8bd6652b974c0a231467216784bf2bb77
dihamza <hamza2rbati@gmail.com>
2022-01-03 17:44:23 +0100

    addition of view file

---
+++ src/main/java/com/application/View/view.java
@@ -0,0 +1,20 @@
+package com.application.View;
+/**
+ * this is not mandatory
+ * it's just to show u the structure wsaf ;)
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class view extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
