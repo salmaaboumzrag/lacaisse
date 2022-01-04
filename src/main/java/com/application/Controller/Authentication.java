42a0583ae6b346e2c9230eaeeb5ee15e536bbbce
hamza nassour <hamza.nassour@edu.uca.ma>
2022-01-04 14:01:00 -0800

    hamza

--- src/main/java/com/application/Controller/Authentication.java
+++ src/main/java/com/application/Controller/Authentication.java
@@ -2,7 +2,7 @@ package com.application.Controller;
 
 /**
  * Proposed by : @Rbati hamza approved by me
- * this class concern the authentication
+ * this class concern the authentication dd
  */
 public class Authentication {
 
