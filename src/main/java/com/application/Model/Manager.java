c25ea41d3793a5c7ad92796f5396dd2d0ef6f57e
dihamza <hamza2rbati@gmail.com>
2022-01-04 23:16:28 +0100

    test

--- src/main/java/com/application/Model/Manager.java
+++ src/main/java/com/application/Model/Manager.java
@@ -1,4 +1,10 @@
 package com.application.Model;
 
-public class Manager {
+import java.util.Date;
+
+public class Manager extends Employee{
+
+    public Manager(int id, String role, String userName, String firstName, String lastName, String email, float salary, String password, int createdBy, Date createdAt, int updatedBy, Date updatedAt) {
+        super(id, role, userName, firstName, lastName, email, salary, password, createdBy, createdAt, updatedBy, updatedAt);
+    }
 }
